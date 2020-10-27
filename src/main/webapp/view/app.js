162742a0cdec2850873441e0f94e7457be9ae802
paramonov <paramonov.alex.v@gmail.com>
2020-10-27 10:17:08 +0300

    material

--- src/main/webapp/view/app.js
+++ src/main/webapp/view/app.js
@@ -1,11 +1,72 @@
-var app = angular.module('app', ['ui.grid', 'ui.grid.pagination', 'ui.bootstrap', 'ngSanitize']);
+var app = angular.module('app', ['ngMaterial', 'md.data.table', 'ngSanitize']);
+app.controller('MsgCtrl', [ '$scope',  'RestService', '$mdEditDialog', function ($scope, RestService, $mdEditDialog) {
+  'use strict';
 
-function RowEditCtrl($scope, $modalInstance, PersonSchema, row) {
+  $scope.selected = [];
+
+  $scope.query = {
+    order: 'created',
+    limit: 5,
+    page: 1,
+    topic: 'lunda_stocks'
+  };
+   RestService.getMssgs($scope.query, success);
+
+  function success(desserts) {
+    $scope.desserts = {data: desserts.data.content, count: desserts.data.totalElements};
+  }
+
+  $scope.getDesserts = function () {
+    $scope.promise = RestService.getMssgs($scope.query, success).$promise;
+  };
+  $scope.view = function (event, dessert) {
+        event.stopPropagation();
+         let editDialog = {
+              modelValue: dessert.message.replace(/\n/g, "<br />"),
+             /* placeholder: 'Add a comment',*/
+              save: function (input) {
+               /* if(input.$modelValue === 'Donald Trump') {
+                  input.$invalid = true;
+                  return $q.reject();
+                }
+                if(input.$modelValue === 'Bernie Sanders') {
+                  return dessert.comment = 'FEEL THE BERN!'
+                }
+                dessert.comment = input.$modelValue;*/
+              },
+              targetEvent: event,
+              title: 'Message',
+              /*validators: {
+                'md-maxlength': 30
+              }*/
+            };
+
+           /* var promise;
+
+            if($scope.options.largeEditDialog) {*/
+              let promise = $mdEditDialog.large(editDialog);
+           /* } else {
+              promise = $mdEditDialog.small(editDialog);
+            }*/
+
+  };
+
+}]);
+
+/*var app = angular.module('app', ['ui.grid', 'ui.grid.pagination', 'ui.bootstrap', 'ngSanitize']);*/
+
+
+/*function RowEditCtrl($scope, $modalInstance, PersonSchema, row) {
     $scope.entity = angular.copy(row.entity);
     $scope.entity.message =  $scope.entity.message.replace(/\n/g, "<br />");
 }
 
 app.controller('MsgCtrl', ['$scope', '$modal', 'RestService', function ($scope, $modal, RestService) {
+    $scope.datepicker = {
+               language: 'ru',
+               format: 'M d, yyyy'
+             }
+
     $scope.paginationOptions = {
         pageNumber: 1,
         pageSize: 5,
@@ -66,9 +127,11 @@ app.controller('MsgCtrl', ['$scope', '$modal', 'RestService', function ($scope, 
         }
     };
 
-}]);
+}]);*/
 
 app.service('RestService', ['$http', function ($http) {
+
+
 
     function getMessages(topic, pageNumber, size) {
         pageNumber = pageNumber > 0 ? pageNumber - 1 : 0;
@@ -86,7 +149,14 @@ app.service('RestService', ['$http', function ($http) {
                 method: 'GET',
                 url: 'api/topics'
             });
-        }
+        },
+        getMssgs: function(query, successCallback) {
+                pageNumber = query.page > 0 ? query.page - 1 : 0;
+                return $http({
+                    method: 'GET',
+                    url: 'api/messages/?topic=' + query.topic + '&page=' + pageNumber + '&size=' + query.limit
+                }).then(successCallback);
+            }
     };
 
 }]);
